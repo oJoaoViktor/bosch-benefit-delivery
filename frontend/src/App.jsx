cb0ed6ff970f367b19c02e472c2a5045f090cc94
Pinott Natalia (SO/BUD3-ETS) <pin3jvl@bosch.com>
2024-10-25 08:16:46 -0300

    :wrench: Config: Realizando a configuração do ambiente FrontEnd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Home from "./pages/home/Home"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GlobalStyles } from "./styles/GlobalStyles"
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path= "/" element = {<Home/>}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./HomeStyles"
+
+const Home = () =>{
+    return(
+        <h1>Hello World</h1>
+    )
+}
+
+export default Home
